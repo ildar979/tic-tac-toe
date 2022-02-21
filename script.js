fb6a6ca419d6746c091a8c2e641ebf5e642af39e
Ildar <ildar9@list.ru>
2022-02-21 20:55:10 +0300

    tic-tac-toe

--- script.js
+++ script.js
@@ -1,58 +1,121 @@
-const cells = document.querySelector(".cells");
-
+let cells = document.querySelector(".cells");
+let res = document.querySelector(".res");
 const bigWindow = document.querySelector(".container");
-const head = document.createElement("div");
-head.className = "head";
-bigWindow.appendChild(head);
-
-  for (let i = 1; i < 9; i++) {
-    let newCell = document.createElement("div");
-    newCell.className = "cell";
-    newCell.id = `${i}`;
-    cells.appendChild(newCell);
-  }
-
-const bash = document.querySelector(".head");
-const selector = document.createElement("select");
-selector.id = "values";
-const option1 = document.createElement("option");
-option1.value = "x";
-option1.innerText = "x";
-selector.appendChild(option1);
-const option2 = document.createElement("option");
-option2.value = "0";
-option2.innerText = "0";
-selector.appendChild(option2);
-
-const input = document.createElement("input");
-input.className = "input";
-input.placeholder = "введите ячеку...";
-//input.innerText = input.value
-
-const btn = document.createElement("button");
+let count = 0;
+let btn = document.createElement("button");
 btn.className = "button";
-btn.innerText = "ввод";
+btn.innerText = "New game";
+bigWindow.appendChild(btn);  
+let step = false;
+ const circle = `<svg class="circle">
+       <circle r="80" cx="125" cy="125" stroke="blue" stroke-width="10"
+               fill="none" stroke-linecap="round" />
+               </svg>`;
+ const cross = `<svg class="cross">
+      <line class="first-line" x1="45" y1="45" x2="200" y2="200" 
+      stroke="red" stroke-width="10" stroke-linecap="round" />
+      <line class="second-line" x1="200" y1="45" x2="45" y2="200" 
+      stroke="red" stroke-width="10" stroke-linecap="round" />
+              </svg>`;
 
-bash.appendChild(selector);
-bash.appendChild(input);
-bash.appendChild(btn);
+function stepCross(target) {
+if(target.tagName == "svg" || target.tagName == "line" || target.tagName == "circle") {
+    return;
+ }
+target.innerHTML = cross;
+target.classList.add("x");
+let crossAudio = new Audio("audio/cross.mp3");
+crossAudio.play();
+  count++;
+};
 
-
-let celll = document.querySelectorAll(".cell");
-
-
-
-
-btn.addEventListener("click", function ticTac(input, celll) {
- 
-//   for (let i = 0; i <= celll.length; i++) {
-//     if (input.value === i) {
-//       console.log(input.value);
-//     }
-//   }
-console.log(input.innerText);
+function stepCircle(target) {
+if(target.tagName == 'svg' || target.tagName == 'line' || target.tagName == 'circle') {
+  return;
 }
-)
-//ticTac(input)
+target.innerHTML = circle;
+target.classList.add("o");
+let circleAudio = new Audio("audio/zero.mp3");
+circleAudio.play();  
+count++;
+}
+
+  for (let i = 1; i < 9; i++) {    
+    let newCell = document.createElement("div");
+    newCell.className = "cell";    
+    cells.appendChild(newCell); 
+
+    function init(elem) {
+      if (!step) stepCross(elem.target);
+      else stepCircle(elem.target);
+      step = !step;
+      win();
+    }
+
+    
+
+function win() {
+  let comb = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+for (let i = 0; i < comb.length; i++) {  
+  let field = document.querySelectorAll(".cell");
+  if (field[comb[i][0]].classList.contains("x") &&
+  field[comb[i][1]].classList.contains("x") && 
+  field[comb[i][2]].classList.contains("x")) {
+     setTimeout(() => {
+      field[comb[i][0]].classList.add("active");
+      field[comb[i][1]].classList.add("active");
+      field[comb[i][2]].classList.add("active");  
+      res.innerText = "Выиграли X";    
+     }, 1500);
+     cells.removeEventListener("click", init);
+   }
+
+   else if (field[comb[i][0]].classList.contains("o") &&
+  field[comb[i][1]].classList.contains("o") && 
+  field[comb[i][2]].classList.contains("o")) {
+     setTimeout(() => {
+      field[comb[i][0]].classList.add("active");
+      field[comb[i][1]].classList.add("active");
+      field[comb[i][2]].classList.add("active");  
+      res.innerText = "Выиграли O";    
+     }, 1500);
+     cells.removeEventListener("click", init);
+   }
+   else if(count === 9) {
+     setTimeout(() => {
+     res.innerText = "Ничья";
+    }, 1500);
+     cells.removeEventListener("click", init);
+   } 
+  }
+ }
+
+ function newGame() {
+  
+  let field = document.querySelectorAll(".cell");
+  step = false;
+  count = 0;
+  res.innerText = "";
+  field.forEach(item => {
+    item.innerText = "";
+    item.classList.remove("x", "o", "active")
+  });  
+cells.addEventListener("click", init);  
+}
+  } 
+
+btn.addEventListener("click", newGame); 
+   
+cells.addEventListener("click", init);
+
 
 
